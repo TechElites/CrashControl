2a471a715f808ef87b63a361a6be5efaf77acfa0
sevetom <sevetom2611@libero.it>
2024-05-21 10:47:08 +0200

    fixed home page ordering

--- app/src/main/java/com/example/crashcontrol/data/database/DAOs.kt
+++ app/src/main/java/com/example/crashcontrol/data/database/DAOs.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CrashesDAO {
-    @Query("SELECT * FROM crash ORDER BY date, time DESC")
+    @Query("SELECT * FROM crash ORDER BY date DESC, time DESC")
     fun getAll(): Flow<List<Crash>>
 
     @Upsert
